72b69c38132228454ee2765ea4233b872586c6f9
whitney shisia <shisiawhitney215@gmail.com>
2024-03-18 20:19:29 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+function randomNumberGreaterThanOne() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+const random = randomNumberGreaterThanOne();
+
+
+const num3 = 9;
+const num4 = 5
+const mod = num3 % num4;
+
+const numbers = [5, 10, 15, 20]; 
+const max = Math.max(...numbers); 
